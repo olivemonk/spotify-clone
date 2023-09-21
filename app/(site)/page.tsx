de5e993255f66c5cc5cdd763861c1722c3bdf703
OliveMonk <gp7.official@gmail.com>
2023-09-21 19:28:10 +0530

    updated home

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -8,10 +8,20 @@ export default function Home() {
         <div className="mb-2">
           <h1 className="text-white text-3xl font-semibold">Welcome Back</h1>
           <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4l-4 gap-3 mt-4">
-            <ListItem image='/images/liked.png' name="Liked Songs" href='liked' />
+            <ListItem
+              image="/images/liked.png"
+              name="Liked Songs"
+              href="liked"
+            />
           </div>
         </div>
       </Header>
+      <div className="mt-2 mb-7 px-6">  
+        <div className="flex justify-between items-center">
+          <h1 className="text-white text-2xl font-semibold">Newest Songs</h1>
+        </div>
+        <div>List of Songs!</div>
+      </div>
     </div>
   );
 }
