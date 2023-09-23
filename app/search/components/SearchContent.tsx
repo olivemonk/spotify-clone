4fb2571dc09d06c543f11286cf20c65ad76f4706
OliveMonk <gp7.official@gmail.com>
2023-09-23 19:12:35 +0530

    added search functionality

---
+++ app/search/components/SearchContent.tsx
@@ -0,0 +1,28 @@
+'use client';
+
+import MediaItem from "@/components/MediaItem";
+import { Song } from "@/types";
+
+interface SearchContentProps {
+    songs: Song[];
+}
+
+const SearchContent:React.FC<SearchContentProps> = ({songs}) => {
+    if(songs.length === 0) {
+        return <div className="flex flex-col gap-y-2 w-full px-6 text-neutral-400">No Songs Found</div>
+    }
+  return (
+    <div className="flex flex-col gap-y-2 w-full px-6">
+        {songs.map((song) => (
+            <div key={song.id} className="flex items-center gap-x-4 w-full">
+                <div className="flex-1">
+                    <MediaItem onClick={()=>{}} data={song} />
+                </div>
+                // Todo add like button here
+            </div>
+        ))}
+    </div>
+  )
+}
+
+export default SearchContent
---
+++ components/SearchInput.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import useDebounce from "@/hooks/useDebounce";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+import qs from "query-string";
+import Input from "./Input";
+
+const SearchInput = () => {
+  const router = useRouter();
+  const [value, setValue] = useState<string>("");
+  const debouncedValue = useDebounce<string>(value, 500);
+
+  useEffect(() => {
+    const query = {
+      title: debouncedValue,
+    };
+
+    const url = qs.stringifyUrl({
+      url: "/search",
+      query,
+    });
+
+    router.push(url);
+  }, [debouncedValue, router]);
+
+  return (
+    <div>
+      <Input
+        placeholder="What do you want to listen?"
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+      />
+    </div>
+  );
+};
+
+export default SearchInput;
